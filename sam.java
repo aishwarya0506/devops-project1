6f69daa3f26fb2702a219d780d55666d463b668c
aishwarya0506 <aish.sri05@gmail.com>
2017-02-27 18:31:14 +0530

    This is my first commit

---
+++ sam.java
@@ -0,0 +1,24 @@
+public class sam {
+	int a=10;
+
+void samp()
+{
+System.out.println("without object");
+}
+static void sample()
+{
+	System.out.println("with object");
+			
+}
+ sam()
+{
+	System.out.println("jjjj");
+}
+public static void main(String args[])
+{
+	sam aishu = new sam();
+	aishu.samp(); 
+	sample();
+	
+}
+}
